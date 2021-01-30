4ea45ff682ba05b6c91acd812517e2def2f8c907
mtzngn <mt.zngn@gmail.com>
2021-01-30 12:10:30 +0000

    'readme'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import styled, { css } from 'styled-components'
 
-function App() {
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>hello world</h1>
+    <Button>Click mee</Button>
+    <Button primary>Click mee</Button>
     </div>
   );
 }
+const Button = styled.button`  background: transparent;
+border-radius: 3px;
+border: 2px solid palevioletred;
+color: palevioletred;
+margin: 0 1em;
+padding: 0.25em 1em;
+
+${props =>
+  props.primary &&
+  css`
+    background: palevioletred;
+    color: white;
+  `};`
 
 export default App;
