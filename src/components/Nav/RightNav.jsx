ea1ad488dc93f583b86e183b6366e0afed8e0d84
mtzngn <mt.zngn@gmail.com>
2021-02-14 13:10:10 +0000

    navbar2 completed

--- src/components/Nav/RightNav.jsx
+++ src/components/Nav/RightNav.jsx
@@ -1,40 +1,62 @@
 import React from 'react'
 import styled from "styled-components";
+import {Link} from "react-router-dom";
+
 
 const Ul = styled.ul `
     list-style: none;
     display: flex;
-    flex-flow: row nowrap;
+    align-items: center;
+
+    flex-flow: column nowrap;
+    background-color: #14213d;
+    position: fixed;
+    transform: ${({ open }) => open ? 'translateX(0)' : 'translateX(100%)'};
+    top: 0;
+    right: 0;
+    height: 100vh;
+    width:300px;
+    padding-top: 5.5rem; 
+    transition: transform 0.3s ease-in-out;
 
     li {
-        padding: 18px 10px;
-    }
-
-    @media (max-width: 768px) {
-        flex-flow: column nowrap;
-        background-color: #0D2538;
-        position: fixed;
-        transform: ${({ open }) => open ? 'translateX(0)' : 'translateX(100%)'};
-        top: 0;
-        right: 0;
-        height: 100vh;
-        width:300px;
-        padding-top: 3.5rem; 
-        transition: transform 0.3s ease-in-out;
-
-        li {
+        padding: 20px 10px;
+        color: #fff;
+        width: 100%;
+        text-align: center;
+        a {
+            text-decoration: none;
             color: #fff;
+            font-size: 1.3em;
         }
     }
+    li:hover {
+        background-color: #e5e5e5;
+        a {
+            color: black;
+        }
+    }
+
+
 `;
 const RightNav = ({ open }) => {
     return (
         <Ul open={open}>
-        <li>Home</li>
-        <li>Projects</li>
-        <li>About Me</li>
-        <li>CV</li>
-        <li>Contact</li>
+      <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/projects">Projects</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/cv">CV</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
     </Ul>
     )
 }
