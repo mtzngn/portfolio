af553db7fd684e82c7022f36577cc30392c0d729
mtzngn <mt.zngn@gmail.com>
2021-02-15 22:34:25 +0000

    project page mainly finished. react spring package used for animation

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const About = () => {
     return (
         <div>
-            <h1 style={{color: "white"}}>this is an about Page</h1>
+            <h1 style={{color: "white"}}>ALMOST READY!</h1>
         </div>
     )
 }
