2bf6ccc1d13014411f5df8f69390bd6846a08261
Taha <mt.zngn@gmail>
2021-02-21 19:22:20 +0000

    contact

--- src/pages/Cv.jsx
+++ src/pages/Cv.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React from 'react';
+import styled from "styled-components";
+
+const StyledDiv = styled.div`
+height:100vh;
+width:100%;
+display: flex;
+justify-content: center;
+align-items: center;
+background-color: #14213D;
+
+
+`
 
 const Cv = () => {
     return (
-        <div>
+        <StyledDiv>
             <h1 style={{color: "white"}}>CV IS COMING SOON!</h1>            
-        </div>
+        </StyledDiv>
     )
 }
 
